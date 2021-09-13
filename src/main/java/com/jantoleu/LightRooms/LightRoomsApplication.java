b5e9488f61751621a42ad1d5e988d7b8088f903a
JohnOldman2018 <zhanbaur@gmail.com>
2021-09-13 17:09:05 +0600

    first commit

---
+++ src/main/java/com/jantoleu/LightRooms/LightRoomsApplication.java
@@ -0,0 +1,13 @@
+package com.jantoleu.LightRooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightRoomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightRoomsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jantoleu/LightRooms/LightRoomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jantoleu.LightRooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightRoomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
