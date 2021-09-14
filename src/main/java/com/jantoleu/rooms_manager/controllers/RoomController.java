37f5d1453eace0f6e5e8522e21db87aee20cb049
JohnOldman2018 <zhanbaur@gmail.com>
2021-09-14 20:30:48 +0600

    pom file changed to standard layout, RoomController and index file modified

--- src/main/java/com/jantoleu/rooms_manager/controllers/RoomController.java
+++ src/main/java/com/jantoleu/rooms_manager/controllers/RoomController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class RoomController {
 
-    @GetMapping("/index")
+    @GetMapping("/index.html")
     public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "index";
