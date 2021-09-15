5f0bee25655f90aa865896cf37d6f261062a2dd0
JohnOldman2018 <zhanbaur@gmail.com>
2021-09-15 18:33:54 +0600

    minor changes

--- src/main/resources/SQL/tables.sql
+++ src/main/resources/SQL/tables.sql
@@ -1,10 +1,10 @@
-CREATE TABLE countries (
+F nCREATE TABLE countries (
     code VARCHAR (2) PRIMARY KEY,
     name VARCHAR (70) NOT NULL
 );
 
 CREATE TABLE rooms (
-    room_ID INTEGER (8) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    room_id INTEGER (8) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     room_name VARCHAR (150) NOT NULL,
     country_code VARCHAR (2) NOT NULL,
     light_is_on TINYINT (0) NOT NULL   
