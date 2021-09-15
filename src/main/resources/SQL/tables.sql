bfe6041246d72132026bcb1dfe25f826cb604dbe
JohnOldman2018 <zhanbaur@gmail.com>
2021-09-15 10:19:56 +0600

    Small change in file for readability

--- src/main/resources/SQL/tables.sql
+++ src/main/resources/SQL/tables.sql
@@ -1,11 +1,11 @@
 CREATE TABLE Countries (
     code VARCHAR (2) PRIMARY KEY,
     name VARCHAR (70) NOT NULL
-    );
+);
 
 CREATE TABLE Rooms (
     room_ID INTEGER (8) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     room_name VARCHAR (150) NOT NULL,
-	country_code VARCHAR (150) NOT NULL,
+    country_code VARCHAR (150) NOT NULL,
     light_is_on BIT DEFAULT 0 NOT NULL   
-    );
+);
