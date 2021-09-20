b0640df8faab3dee58e8dc04b3f9d987464f7fa5
JohnOldman2018 <zhanbaur@gmail.com>
2021-09-20 17:59:32 +0600

    Get room test

--- src/main/java/com/jantoleu/rooms_manager/Application.java
+++ src/main/java/com/jantoleu/rooms_manager/Application.java
@@ -2,7 +2,9 @@ package com.jantoleu.rooms_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJdbcRepositories
@@ -10,6 +12,11 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate () {
+		return new RestTemplate();
 	}
 
 }
